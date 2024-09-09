4078077eb2afb3a52376987d300a2673284c9f22
Samuli Martikainen <smartikainen_@outlook.com>
2024-09-09 22:30:20 +0300

    add some todos

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import 'react-native-get-random-values';
 import 'expo-router/entry';
+
+/*
+TODO
+- Leverage ja ownership ratio
+- Korjaa käyttäjätietojen muokkauksen bottomsheet
+- bottomsheetit omiks komponenteiks provider hommelin sijaan?
+*/
